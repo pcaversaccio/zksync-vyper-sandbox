e70acfe3884ce460c2e5e32eeb897198d6972706
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-12 08:11:17 +0100

    ♻️ Cleanup
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 999999,
+        runs: 999_999,
       },
       evmVersion: "paris", // prevent using the `PUSH0` opcode
     },
