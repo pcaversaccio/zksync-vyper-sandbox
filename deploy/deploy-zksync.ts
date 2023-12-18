516ed0ab8fba3072feffec0b67b8510e32668769
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-18 14:39:54 +0100

    ♻️  Chore
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- deploy/deploy-zksync.ts
+++ deploy/deploy-zksync.ts
@@ -3,6 +3,10 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Wallet } from "zksync-ethers";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
+// Colour codes for terminal prints
+const RESET = "\x1b[0m";
+const GREEN = "\x1b[32m";
+
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -42,9 +46,12 @@ export default async function main(hre: HardhatRuntimeEnvironment) {
   await contract.waitForDeployment();
   const contractAddress = await contract.getAddress();
 
-  console.log("Foo deployed to:", contractAddress);
+  console.log("Foo deployed to: " + `${GREEN}${contractAddress}${RESET}\n`);
 
   if (!localFlag) {
+    console.log(
+      "Waiting 30 seconds before beginning the contract verification to allow the block explorer to index the contract...\n",
+    );
     await delay(30000); // Wait for 30 seconds before verifying the contract
 
     await hre.run("verify:verify:vyper", {
