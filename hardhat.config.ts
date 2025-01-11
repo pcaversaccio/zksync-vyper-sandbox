a2f836460285d68f47f005af1ff5d8236185b9c7
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2025-01-11 18:05:33 +0100

    🔁 Bump Dependencies
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -45,7 +45,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.5.8",
+    version: "1.5.9",
     compilerSource: "binary",
     settings: {
       enableEraVMExtensions: false,
@@ -58,7 +58,7 @@ const config: HardhatUserConfig = {
     },
   },
   zkvyper: {
-    version: "1.5.8",
+    version: "1.5.9",
     settings: {
       libraries: {},
       optimizer: {
